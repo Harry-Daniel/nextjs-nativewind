bd558af32fb8e8ae519f1b1d63be09091d81a79e
Harry-Daniel <hayiborkomla@gmail.com>
2025-08-22 15:24:11 +0000

    Tailwind is onboard, setting up the navigation now

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,3 +1,4 @@
+import "../global.css"
 import { Slot } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+
+const { width, height } = Dimensions.get("screen");
 
 const Home = () => {
   return (
-    <View>
-      <Text>hhHome</Text>
+    <View
+      className="justify-center items-center"
+      style={{ width: width, height: height }}
+    >
+      <View>
+        <Text className="text-gray-500">Welcome to the Home Page!</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
